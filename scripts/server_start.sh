69d6f1cd3a85fcb1fc35b8bed750b935ae537a40
nextjedi <arunabhmaster@gmail.com>
2023-03-07 01:00:24 +0530

    jar to war

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo /usr/bin/java -war -Dserver.port=80 \
+    *.war > /dev/null 2> /dev/null < /dev/null &
